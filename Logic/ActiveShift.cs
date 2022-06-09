1c21506a80adba197ee400865f079af65faba526
dulipub <dulipub@gmail.com>
2022-06-09 19:05:34 +0530

    logic test module

---
+++ Logic/ActiveShift.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Logic
+{
+    public class ActiveShift
+    {
+        /// <summary>
+        /// Calculate if there is a shift. Assuming shift last less than 12 hrs.
+        /// </summary>
+        /// <param name="time">Time to evaluate</param>
+        /// <param name="shiftStart">Start time of shift give as HH.MM AM or PM </param>
+        /// <param name="shiftEnd">End time of shift give as HH.MM AM or PM </param>
+        /// <returns>True or false</returns>
+        public bool IsActiveShift(DateTime currentTime, string shiftStart, string shiftEnd)
+        {
+            bool isActive = false;
+            char[] delimeters = new char[] { '.', ' ' };
+
+            string[] startTimeSplit = shiftStart.Split(delimeters);
+            string[] endTimeSplit = shiftEnd.Split(delimeters);
+
+            DateTime startDateTime = currentTime.Date;
+            DateTime endDateTime = currentTime.Date;
+
+            TimeSpan midDay = new TimeSpan(12, 0, 0);
+            if (startTimeSplit[startTimeSplit.Length - 1] == "PM" && endTimeSplit[endTimeSplit.Length - 1] == "AM" && currentTime.TimeOfDay > midDay)
+            {
+                endDateTime = currentTime.Date.AddDays(1);
+            }
+            else if (startTimeSplit[startTimeSplit.Length - 1] == "PM" && endTimeSplit[endTimeSplit.Length - 1] == "AM" && currentTime.TimeOfDay < midDay)
+            {
+                startDateTime = currentTime.Date.AddDays(-1);
+            }
+
+
+            startDateTime = AddTime(startDateTime, startTimeSplit);
+            endDateTime = AddTime(endDateTime, endTimeSplit);
+
+            if (currentTime >= startDateTime && currentTime <= endDateTime)
+            {
+                isActive = true;
+            }
+
+            return isActive;
+        }
+
+        private DateTime AddTime(DateTime date, string[] split)
+        {
+            DateTime modified = DateTime.Now;
+            int hours = int.Parse(split[0]);
+            int mins = 0;
+            if (split[split.Length - 1] == "PM")
+            {
+                hours += 12;
+            }
+
+            if (split.Length == 3)
+            {
+                mins = int.Parse(split[1]);
+            }
+
+            modified = date.AddHours(hours).AddMinutes(mins);
+            return modified;
+        }
+    }
+}
---
+++ Logic/ActiveShiftTests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Xunit;
+
+namespace Logic
+{
+    public class ActiveShiftTests
+    {
+        [Fact]
+        public void IsActiveShift_PM_To_AM_True()
+        {
+            ActiveShift shift = new ActiveShift();
+
+            bool expected = true;
+            DateTime date = new DateTime(2020, 7, 22, 6, 0, 0);
+            bool actual = shift.IsActiveShift(date, "10 PM", "6.00 AM");
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void IsActiveShift_PM_To_AM_False()
+        {
+            ActiveShift shift = new ActiveShift();
+
+            bool expected = false;
+            DateTime date = new DateTime(2020, 7, 22, 7, 0, 0);
+            bool actual = shift.IsActiveShift(date, "10 PM", "6.00 AM");
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void IsActiveShift_AM_To_PM_False()
+        {
+            ActiveShift shift = new ActiveShift();
+
+            bool expected = false;
+            DateTime date = new DateTime(2020, 7, 22, 7, 0, 0);
+            bool actual = shift.IsActiveShift(date, "9 AM", "6.30 PM");
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void IsActiveShift_AM_To_PM_True()
+        {
+            ActiveShift shift = new ActiveShift();
+
+            bool expected = true;
+            DateTime date = new DateTime(2020, 7, 22, 9, 1, 0);
+            bool actual = shift.IsActiveShift(date, "9 AM", "6.30 PM");
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void IsActiveShift_PM_To_PM_True()
+        {
+            ActiveShift shift = new ActiveShift();
+
+            bool expected = true;
+            DateTime date = new DateTime(2020, 7, 22, 22, 0, 0);
+            bool actual = shift.IsActiveShift(date, "6 PM", "11.59 PM");
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void IsActiveShift_PM_To_PM_Fale()
+        {
+            ActiveShift shift = new ActiveShift();
+
+            bool expected = false;
+            DateTime date = new DateTime(2020, 7, 22, 16, 59, 0);
+            bool actual = shift.IsActiveShift(date, "6 PM", "11.59 PM");
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void IsActiveShift_AM_To_AM_True()
+        {
+            ActiveShift shift = new ActiveShift();
+
+            bool expected = true;
+            DateTime date = new DateTime(2020, 7, 22, 9, 1, 0);
+            bool actual = shift.IsActiveShift(date, "9 AM", "12.00 AM");
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void IsActiveShift_AM_To_AM_False()
+        {
+            ActiveShift shift = new ActiveShift();
+
+            bool expected = false;
+            DateTime date = new DateTime(2020, 7, 22, 8, 1, 0);
+            bool actual = shift.IsActiveShift(date, "9 AM", "12.00 AM");
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
